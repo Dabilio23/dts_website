71b04c1f0c511f14acaefef62f86b9e46f6cb685
“Mahanina” <mahanina@dabil.io>
2026-03-09 15:39:03 +0300

    amélioration SEO

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -11,10 +11,25 @@ export default function Layout({ children, title, description }) {
   }, [pathname])
 
   useEffect(() => {
-    if (title) document.title = `${title} | DTS`
-    const meta = document.querySelector('meta[name="description"]')
-    if (meta && description) meta.setAttribute('content', description)
-  }, [title, description])
+    const BASE_URL = 'https://dts.dabil.io'
+    const fullTitle = title ? `${title} | DTS` : 'DTS | Design to scale'
+    const fullUrl = `${BASE_URL}${pathname}`
+
+    document.title = fullTitle
+
+    const setMeta = (selector, attr, value) => {
+      const el = document.querySelector(selector)
+      if (el && value) el.setAttribute(attr, value)
+    }
+
+    setMeta('meta[name="description"]', 'content', description)
+    setMeta('link[rel="canonical"]', 'href', fullUrl)
+    setMeta('meta[property="og:title"]', 'content', fullTitle)
+    setMeta('meta[property="og:description"]', 'content', description)
+    setMeta('meta[property="og:url"]', 'content', fullUrl)
+    setMeta('meta[name="twitter:title"]', 'content', fullTitle)
+    setMeta('meta[name="twitter:description"]', 'content', description)
+  }, [title, description, pathname])
 
   return (
     <div className="antialiased selection:bg-primary-500/30 selection:text-white">
